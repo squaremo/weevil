fd3018e20e725a0a76e8fd5b3e5cff449aa7ad21
Michael Bridgen <mikeb@squaremobius.net>
2015-10-05 13:03:01 +0100

    Serve static files

--- server/main.go
+++ server/main.go
@@ -9,8 +9,14 @@ import (
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/{service}/", listInstances)
-	router.HandleFunc("/", listServices)
+
+	router.HandleFunc("/", homePage)
+	router.HandleFunc("/index.html", homePage)
+	router.PathPrefix("/res/").HandlerFunc(handleResource)
+
+	router.HandleFunc("/api/{service}/", listInstances)
+	router.HandleFunc("/api/", listServices)
+
 	http.ListenAndServe("0.0.0.0:7070", router)
 }
 
@@ -20,4 +26,13 @@ func listServices(w http.ResponseWriter, r *http.Request) {
 
 func listInstances(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{})
+}
+
+func handleResource(w http.ResponseWriter, r *http.Request) {
+	file := r.URL.Path[1:]
+	http.ServeFile(w, r, file)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
 }
